c78adceff121202eb1f15f8d17e0a7730f0d66a1
Ayubxon2009 <abduganieva200980@gmail.com>
2025-02-10 16:05:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let name = prompt(`Ism sharifingizni kiriting` )
+
+let school = prompt(`nechinchi mak-tabda o'qiysiz`)
+
+let ustoz = prompt(`ustozingizni ismini kiriting`)
+
+let natija = `Mening ismim sharifim ${name}. Men ${school}-maktabda o'qiyman. Mars IT Schoolda ${ustoz}da o'qiyman`
+
+ alert(natija)
